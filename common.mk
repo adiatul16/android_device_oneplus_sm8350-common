4e9b72a648d562cd6a44a3789c4550d4384204f6
TogoFire <togofire@mailfence.com>
2025-08-07 04:27:43 +0000

    sm8350-common: Enable freeform windows Signed-off-by: Pranav Vashi <neobuddy89@gmail.com> Change-Id: I90d117dfbcc3b0cc759d7bdc193d307ba9022d03 Signed-off-by: mukesh22584 <mks22584@gmail.com> (cherry picked from commit 2e455b66dde97448192d56c0f556bfbab59e2bbf) Signed-off-by: TogoFire <togofire@mailfence.com> Signed-off-by: adiatul16 <adiatul16@gmail.com>
    Signed-off-by: adiatul16 <adiatul16@gmail.com>

--- common.mk
+++ common.mk
@@ -143,6 +143,10 @@ PRODUCT_COPY_FILES += \
 
 $(call soong_config_set,surfaceflinger,udfps_lib,//hardware/oplus:libudfps_extension.oplus)
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # FOD
 TARGET_HAS_UDFPS := true
 
