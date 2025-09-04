PixelOS-Devices/device_oneplus_sm8350-common

sm8350-common: Import Dolby C2 Media service
from OnePlus 11 CPH2447_15.0.0.401(EX01)

Change-Id: Icdfc5b11c7635d0707c2a39f54e0c8d149c771bc

extract-files.py
@@ -71,6 +71,24 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('"max_retry_alloc_output_timeout": 1000,', '"max_retry_alloc_output_timeout": 0,'),
     'vendor/etc/msm_irqbalance.conf': blob_fixup()
         .regex_replace('IGNORED_IRQ=27,23,38$', 'IGNORED_IRQ=27,23,38,115,332'),
+    # Dolby START
+    'odm/bin/hw/vendor.dolby_sp.media.c2@1.0-service': blob_fixup()
+        .replace_needed('libcodec2_hidl@1.0.so', 'libcodec2_hidl@1.0_sp.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk_sp.so'),
+    ('odm/lib64/libcodec2_hidl_plugin_sp.so', 'odm/lib64/libcodec2_soft_common_sp.so', 'odm/lib64/libcodec2_store_dolby_sp.so'): blob_fixup()
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk_sp.so'),
+    ('odm/lib64/libcodec2_soft_ac4dec_sp.so', 'odm/lib64/libcodec2_soft_ddpdec_sp.so'): blob_fixup()
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk_sp.so')
+        .replace_needed('libcodec2_soft_common.so', 'libcodec2_soft_common_sp.so'),
+    'odm/lib64/libcodec2_vndk_sp.so': blob_fixup()
+        .replace_needed('libui.so', 'libui_sp.so'),
+    'odm/lib64/libcodec2_hidl@1.0_sp.so': blob_fixup()
+        .replace_needed('libcodec2_hidl_plugin.so', 'libcodec2_hidl_plugin_sp.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk_sp.so'),
+    'odm/lib64/libui_sp.so': blob_fixup()
+        .replace_needed('android.hardware.graphics.common-V3-ndk.so', 'android.hardware.graphics.common-V5-ndk.so')
+        .replace_needed('android.hardware.graphics.allocator-V1-ndk.so', 'android.hardware.graphics.allocator-V2-ndk.so'),
+    # Dolby END
 }  # fmt: skip
 
 module = ExtractUtilsModule(

adiatul16/android_device_oneplus_sm8350-common

extract-files.py
@@ -59,6 +59,24 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('"max_retry_alloc_output_timeout": 1000,', '"max_retry_alloc_output_timeout": 0,'),
     'vendor/etc/msm_irqbalance.conf': blob_fixup()
         .regex_replace('IGNORED_IRQ=27,23,38$', 'IGNORED_IRQ=27,23,38,115,332'),
+    # Dolby START
+    'odm/bin/hw/vendor.dolby_sp.media.c2@1.0-service': blob_fixup()
+        .replace_needed('libcodec2_hidl@1.0.so', 'libcodec2_hidl@1.0_sp.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk_sp.so'),
+    ('odm/lib64/libcodec2_hidl_plugin_sp.so', 'odm/lib64/libcodec2_soft_common_sp.so', 'odm/lib64/libcodec2_store_dolby_sp.so'): blob_fixup()
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk_sp.so'),
+    ('odm/lib64/libcodec2_soft_ac4dec_sp.so', 'odm/lib64/libcodec2_soft_ddpdec_sp.so'): blob_fixup()
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk_sp.so')
+        .replace_needed('libcodec2_soft_common.so', 'libcodec2_soft_common_sp.so'),
+    'odm/lib64/libcodec2_vndk_sp.so': blob_fixup()
+        .replace_needed('libui.so', 'libui_sp.so'),
+    'odm/lib64/libcodec2_hidl@1.0_sp.so': blob_fixup()
+        .replace_needed('libcodec2_hidl_plugin.so', 'libcodec2_hidl_plugin_sp.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk_sp.so'),
+    'odm/lib64/libui_sp.so': blob_fixup()
+        .replace_needed('android.hardware.graphics.common-V3-ndk.so', 'android.hardware.graphics.common-V6-ndk.so')
+        .replace_needed('android.hardware.graphics.allocator-V1-ndk.so', 'android.hardware.graphics.allocator-V2-ndk.so'),
+    # Dolby END
 }  # fmt: skip
 
 module = ExtractUtilsModule(