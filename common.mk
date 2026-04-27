d2f50ea08fe269996918591b421a82e99f3e8b9b
Jabiyeff <cebiyevanar@gmail.com>
2026-04-27 17:50:39 +0100

    sm8350-common: Force disable iorapd * Its eating more RAM and slow down device * Thanks for CAFEBABE find this
    Signed-off-by: adiatul16 <adiatul16@gmail.com>

--- common.mk
+++ common.mk
@@ -263,6 +263,11 @@ PRODUCT_COPY_FILES += \
 
 $(call soong_config_set,qtipower,tap_to_wake_node,/proc/touchpanel/double_tap_enable)
 
+# Powerhal properties
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.iorapd.enable=false \
+    iorapd.perfetto.enable=false
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.oplus-multihal \
